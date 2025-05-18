bdf051ca0d1d7394f27476411ce40bebe79c2bcc
Ashish Rathi <rathiashish4321@gmail.com>
2025-05-18 11:52:31 +0530

    ...

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+ if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+
 function toggleTheme() {
   const body = document.body;
   const icon = document.getElementById("themeToggle").firstElementChild;
@@ -7,13 +11,42 @@ function toggleTheme() {
   if (body.classList.contains("dark-mode")) {
     icon.classList.remove("fa-moon");
     icon.classList.add("fa-sun");
+    localStorage.setItem("theme", "dark");
   } else {
     icon.classList.remove("fa-sun");
     icon.classList.add("fa-moon");
+    localStorage.setItem("theme", "light");
   }
-  
 }
 
+window.onload = function () {
+  const body = document.body;
+  const icon = document.getElementById("themeToggle").firstElementChild;
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    body.classList.add("dark-mode");
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  } else {
+    body.classList.remove("dark-mode");
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  }
+}
+
+
+ window.onload = function () {
+    // Get current page filename
+    const currentPage = window.location.pathname.split("/").pop();
+
+    // Set active class
+    if (currentPage === "index.html" || currentPage === "") {
+      document.getElementById("nav-home").classList.add("active");
+    } else if (currentPage === "about.html") {
+      document.getElementById("nav-about").classList.add("active");
+    }
+  };
 
 
 const next = document.querySelector('#next');
