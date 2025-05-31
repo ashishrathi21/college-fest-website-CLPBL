c99ed679ee2ae0f5b7361b3f6dc77650738e74ff
Ashish Rathi <rathiashish4321@gmail.com>
2025-05-31 18:40:38 +0530

    .....

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Event(models.Model):
-    title = models.CharField(max_length=100)
-    date = models.DateField()
-    time = models.TimeField()
-    image = models.ImageField(upload_to='mainapp/images/scheduleImages/')
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='event_images/')  # Images yahan save hongi
 
     def __str__(self):
-        return self.title
+        return self.name
+
 
 
 
